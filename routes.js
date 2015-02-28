47930e9085671e532e4aa93c999eee2f6f9c7a5a
Kush Patel <kushpatel@outlook.com>
2015-02-28 16:57:24 -0600

    added result page

--- routes.js
+++ routes.js
@@ -17,7 +17,12 @@ module.exports = function(app, passport) {
     var KEY2 = 'key2';
     var publicKey = jsencrypt.getPublicKey();
     var privateKey = jsencrypt.getPrivateKey();
-    var plainHash, keyHash, pkiHash, passwordHash, sessionHash, twoHash; 
+    var plainHash = false, 
+        keyHash = false, 
+        pkiHash = false, 
+        passwordHash = false, 
+        sessionHash = false, 
+        twoHash = false; 
 
     // =====================================
     // HOME PAGE (with login links) ========
@@ -56,7 +61,7 @@ module.exports = function(app, passport) {
 
     app.post('/profile', isLoggedIn, function(req, res) {
         algorithms = req.body.algorithm;
-        console.log(algorithms);
+        //console.log(algorithms);
         res.redirect('/item');
     });
 
@@ -96,42 +101,59 @@ module.exports = function(app, passport) {
             startTime = Date.now();
             if (_.indexOf(algorithms, '1') !== -1) {
                 plainHash = checkPlainHash(ALGORITHM1, body, req.headers.plainhash);
-                verificationMessage.concat("Plain Hash verified: " + plainHash + "\n");
+                //verificationMessage.concat("Plain Hash verified: " + plainHash + "\n");
             }
             if (_.indexOf(algorithms, '2') !== -1) {
                 keyHash = checkKeyHash(ALGORITHM1, KEY1, body, req.headers.keyhash);
-                verificationMessage.concat("Hash with security key verified: " + keyHash + "\n");
+                //verificationMessage.concat("Hash with security key verified: " + keyHash + "\n");
             }
             if (_.indexOf(algorithms, '3') !== -1) {
                 var symkey = jsencrypt.decrypt(req.headers.symkey);
                 pkiHash = checkPkiHash(ALGORITHM1, symkey, body, req.headers.pkihash);
-                verificationMessage.concat("Hash with PKI encryption verified: " + pkiHash + "\n");
+                //verificationMessage.concat("Hash with PKI encryption verified: " + pkiHash + "\n");
             }
             if (_.indexOf(algorithms, '4') !== -1) {
                 var post = qs.parse(body);
                 if(req.user.password == post.password) {
                     passwordHash = checkPasswordHash(ALGORITHM1, post.password, body, req.headers.passwordhash);
-                    verificationMessage.concat("Hash with Password key verified: " + passwordHash + "\n");
+                    //verificationMessage.concat("Hash with Password key verified: " + passwordHash + "\n");
                 } else {
                     res.end("password incorrect");
                 }
             }
             if (_.indexOf(algorithms, '5') !== -1) {
                 sessionHash = checkSessionHash(ALGORITHM1, req.sessionID, body, req.headers.sessionhash);
-                verificationMessage.concat("Hash with Session verified: " + sessionHash + "\n");
+                //verificationMessage.concat("Hash with Session verified: " + sessionHash + "\n");
             }
             if (_.indexOf(algorithms, '6') !== -1) {
                 twoHash = checkTwoHash(ALGORITHM1, ALGORITHM2, KEY1, KEY2, body, req.headers.firsthash, req.headers.secondhash);
-                verificationMessage.concat("Hash with 2 Hash, 2 Key verified: " + twoHash + "\n");
+                //verificationMessage.concat("Hash with 2 Hash, 2 Key verified: " + twoHash + "\n");
             }
             endTime = Date.now();
         });
-        console.log(verificationMessage);
+        var post = qs.parse(body);
+        
         processingTime = endTime - startTime;
-        console.log(processingTime);
-        res.end(verificationMessage.concat("Server Prossecing Time: " + processingTime.toString() + " msec"));
+        //console.log(processingTime);
+        res.redirect('/result');
+        //res.end(verificationMessage.concat("Server Prossecing Time: " + processingTime.toString() + " msec"));
     });
 
+    // =====================================
+    // RESULT ==============================
+    // =====================================
+    app.get('/result', function(req, res) {
+        res.render('result.jade', {
+            user : req.user, // get the user out of session and pass to template
+            plainhash: plainHash,
+            keyhash: keyHash, 
+            pkihash: pkiHash, 
+            passwordhash: passwordHash, 
+            sessionhash: sessionHash, 
+            twohash: twoHash,
+            time: processingTime.toString()
+        });
+    });
     // =====================================
     // LOGOUT ==============================
     // =====================================
@@ -154,79 +176,79 @@ function isLoggedIn(req, res, next) {
 
 
 function checkPlainHash(algorithm, data, headerHash) {
-    console.log("\n**************** Integrity Check method: Plain Hash ****************");
-    console.log('plainhash: ', headerHash);
+    //console.log("\n**************** Integrity Check method: Plain Hash ****************");
+    //console.log('plainhash: ', headerHash);
     var hash = crypto.createHash(algorithm).update(data).digest('hex');
-    console.log('plainhash generated: ', hash);
+    //console.log('plainhash generated: ', hash);
     return hash == headerHash;
 }
 
 function checkKeyHash(algorithm, key, data, headerHash) {
-    console.log("\n**************** Integrity Check method: Hash with security key ****************");
-    console.log('keyhash: ', headerHash);
+    //console.log("\n**************** Integrity Check method: Hash with security key ****************");
+    //console.log('keyhash: ', headerHash);
     var hash = crypto.createHmac(algorithm, key);
     hash.setEncoding('hex');
     hash.write(data);
     hash.end();
     hash = hash.read();
-    console.log('keyhash generated: ', hash);
+    //console.log('keyhash generated: ', hash);
     return hash == headerHash;
 }
 
 function checkPkiHash(algorithm, key, data, headerHash) {
-    console.log("\n**************** Integrity Check method: Hash with security key encrypted with PKI ****************");
-    console.log('pkihash: ', headerHash);
+    //console.log("\n**************** Integrity Check method: Hash with security key encrypted with PKI ****************");
+    //console.log('pkihash: ', headerHash);
     var hash = crypto.createHmac(algorithm, key);
     hash.setEncoding('hex');
     hash.write(data);
     hash.end();
     hash = hash.read();
-    console.log('pkihash generated: ', hash);
+    //console.log('pkihash generated: ', hash);
     return hash == headerHash;
 }
 
 function checkPasswordHash(algorithm, key, data, headerHash) {
-    console.log("\n**************** Integrity Check method: Hash with password ****************");
-    console.log('passwordhash: ', headerHash);
+    //console.log("\n**************** Integrity Check method: Hash with password ****************");
+    //console.log('passwordhash: ', headerHash);
     var hash = crypto.createHmac(algorithm, key);
     hash.setEncoding('hex');
     hash.write(data);
     hash.end();
     hash = hash.read();
-    console.log('passwordhash generated: ', hash);
+    //console.log('passwordhash generated: ', hash);
     return hash == headerHash;
 }
 
 function checkSessionHash(algorithm, key, data, headerHash) {
-    console.log("\n**************** Integrity Check method: Hash with session ****************");
-    console.log('sessionhash: ', headerHash);
+    //console.log("\n**************** Integrity Check method: Hash with session ****************");
+    //console.log('sessionhash: ', headerHash);
     var hash = crypto.createHmac(algorithm, key);
     hash.setEncoding('hex');
     hash.write(data);
     hash.end();
     hash = hash.read();
-    console.log('sessionhash generated: ', hash);
+    //console.log('sessionhash generated: ', hash);
     return hash == headerHash;
 }
 
 function checkTwoHash(algorithm1, algorithm2, key1, key2, data, headerHash1, headerHash2) {
-    console.log("\n**************** Integrity Check method: 2 hash, 2 key, 2 algorithms ****************");
+    //console.log("\n**************** Integrity Check method: 2 hash, 2 key, 2 algorithms ****************");
     
-    console.log('first hash: ', headerHash1);
+    //console.log('first hash: ', headerHash1);
     var hash1 = crypto.createHmac(algorithm1, key1);
     hash1.setEncoding('hex');
     hash1.write(data);
     hash1.end();
     hash1 = hash1.read();
-    console.log('first hash generated: ', hash1);
+    //console.log('first hash generated: ', hash1);
 
-    console.log('second hash: ', headerHash2);
+    //console.log('second hash: ', headerHash2);
     var hash2 = crypto.createHmac(algorithm2, key2);
     hash2.setEncoding('hex');
     hash2.write(data);
     hash2.end();
     hash2 = hash2.read();
-    console.log('second hash generated: ', hash2);
+    //console.log('second hash generated: ', hash2);
     
     return (hash1 == headerHash1 && hash2 == headerHash2);
 }
